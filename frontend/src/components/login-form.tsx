0debe7dd2a689a2b1a9f813e0428c71fb94a8ced
Wilmer Guzman <joelguzman2468@gmail.com>
2024-05-25 22:15:52 -0400

    Initial commit

---
+++ frontend/src/components/login-form.tsx
@@ -0,0 +1,16 @@
+export default function LoginForm() {
+  return (
+    <>
+      <form>
+        <div>
+          <label>Username</label>
+          <input></input>
+        </div>
+        <div>
+          <label>Password</label>
+          <input></input>
+        </div>
+      </form>
+    </>
+  );
+}
