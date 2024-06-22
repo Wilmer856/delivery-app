d552149f18fec002f7ea344d2562a48faf232960
Wilmer856 <joelguzman2468@gmail.com>
2024-06-22 10:57:38 -0400

    Created setup for deliveries context

---
+++ frontend/src/context/deliveriesContext.tsx
@@ -0,0 +1,18 @@
+// import { createContext, useReducer } from "react";
+
+
+// export const DeliveriesContext = createContext();
+
+// export const deliveriesReducer = (state, action) => {
+
+// }
+
+// export const DeliveriesContextProvider = ({ children }) => {
+
+//     return (
+//         <DeliveriesContext.Provider value={}>
+
+//         </DeliveriesContext.Provider>
+//     )
+// }
+
